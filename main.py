f256cb5d6d14b89c5080b40d37095bccf994a258
Muhammadamin Mamadaliyev <91719420+mamadaliyev66@users.noreply.github.com>
2022-09-23 01:38:48 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument, wrong-import-position
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+from random import random
+import re
+
+from typing import Dict
+from telegram import __version__ as TG_VER
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [
+    ["Next➡️"],
+    ["Answer✅"],
+
+]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def facts_to_str(user_data: Dict[str, str]) -> str:
+    """Helper function for formatting the gathered user info."""
+    facts = [f"{key} - {value}" for key, value in user_data.items()]
+    return "\n".join(facts).join(["\n", "\n"])
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Start the conversation and ask user for input."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Well Come {user.mention_html()}!"
+        "\nPress Next➡️ Button For Start Questions ",
+        reply_markup=markup,
+    )
+
+    return CHOOSING
+import pandas as pd
+from random import randint
+excel_data_df = pd.read_excel('questions.xlsx')
+question=excel_data_df['questions'].tolist()
+b=0
+async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Ask the user for info about the selected predefined choice."""
+    text = update.message.text
+    from random import randint,choice
+    global b
+    b=randint(0,len(excel_data_df)-2)
+    context.user_data["choice"] = text
+    await update.message.reply_text(question[b])
+    return CHOOSING
+
+
+
+
+answer=excel_data_df['answer'].tolist()
+
+async def custom_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Display the gathered info and end the conversation."""
+    user_data = context.user_data
+    if "choice" in user_data:
+        del user_data["choice"]
+
+    await update.message.reply_text(
+        answer[b],
+        
+    )
+async def typing_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user_data = context.user_data
+    global b
+    
+    if "choice" in user_data:
+        del user_data["choice"]
+
+    await update.message.reply_text(
+        "typing choice",
+        
+    )
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("5632292395:AAGe-bVNa1zpTMlq6k_iPfvmeo_DxZj6Xyc").build()
+
+    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            CHOOSING: [
+                MessageHandler(
+                    filters.Regex("^(Next➡️)$"), regular_choice
+                ),
+                MessageHandler(filters.Regex("^Answer✅$"), custom_choice),
+            ],
+            TYPING_CHOICE: [
+                MessageHandler(
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), regular_choice
+                )
+            ],
+            
+            
+        },
+    
+        fallbacks=[MessageHandler(filters.Regex("^Done$"), custom_choice)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
